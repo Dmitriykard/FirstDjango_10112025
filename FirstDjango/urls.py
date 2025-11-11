d470213aacc3116b01a81ffe9091c5b6fb4c5823
Dmitry Kardash <kardashd@mail.ru>
2025-11-11 16:52:57 +0300

    Practice

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from MainApp import views
-
+from MainApp import views  
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),  
+    path('about/', views.about, name='about'),  
+    path('item/<int:item_id>/', views.item_detail, name='item_detail'),  
+    path('items/', views.all_items, name='all_items'),  
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,11 +1,62 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here
+author = {
+    'first_name': 'Николай',
+    'middle_name': 'Петрович',
+    'last_name': 'Сидоров',
+    'phone': '8-914-544-20-30',
+    'email': 'kolay_sidor12@mail.ru'
+}
+
 
 def home(request):
     text = """
-    <h1>"Изучаем django"</h1>
-    <strong>Автор</strong>: <i>Иванов И.П.</i>
-    """ 
+    <h1>Изучаем django</h1>
+    <strong>Автор</strong>: <i>Сидоров Н.И.</i>
+    """
     return HttpResponse(text)
+
+
+def about(request):
+    html_response = f"""
+    Имя: {author['first_name']}<br/>
+    Отчество: {author['middle_name']}<br/>
+    Фамилия: {author['last_name']}<br/>
+    Телефон: {author['phone']}<br/>
+    Email: {author['email']}
+    """
+    return HttpResponse(html_response)
+
+
+items = [
+    {"id": 1, "name": "Кроссовки abibas"},
+    {"id": 2, "name": "Куртка кожаная"},
+    {"id": 3, "name": "Coca-cola 1 литр"},
+    {"id": 4, "name": "Картофель фри"},
+    {"id": 5, "name": "Кепка"}
+]
+
+
+def item_detail(request, item_id):
+    found_item = None
+    for item in items:
+        if item["id"] == int(item_id):
+            found_item = item
+            break
+    
+    if found_item is not None:
+        return HttpResponse(
+            f"<h1>{found_item['name']}</h1><p><a href='/items/'>Назад к списку товаров</a></p>"
+        )
+    else:
+        return HttpResponse("Товар не найден")
+
+
+def all_items(request):
+    response_html = "<h1>Список товаров</h1><ol>"
+    for item in items:
+        response_html += f"<li><a href='/item/{item['id']}/'>{item['name']}</a></li>"
+    response_html += "</ol>"
+    return HttpResponse(response_html)
