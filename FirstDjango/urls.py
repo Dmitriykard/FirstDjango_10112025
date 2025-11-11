eed736cf8e73abdd6255345b9046c4b59b40ec57
Dmitry Kardash <kardashd@mail.ru>
2025-11-11 20:51:32 +0300

    Working project

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -1,9 +1,11 @@
+from django.contrib import admin
 from django.urls import path
 from MainApp import views  
 
 urlpatterns = [
-    path('', views.home, name='home'),  
-    path('about/', views.about, name='about'),  
-    path('item/<int:item_id>/', views.item_detail, name='item_detail'),  
-    path('items/', views.all_items, name='all_items'),  
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('item/<int:item_id>/', views.item_detail, name='item_detail'),
+    path('items/', views.all_items, name='all_items'),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -10,25 +10,6 @@ author = {
     'email': 'kolay_sidor12@mail.ru'
 }
 
-# Главная страница
-def home(request):
-    text = """
-    <h1>Изучаем django</h1>
-    <strong>Автор</strong>: <i>Сидоров Н.И.</i>
-    """
-    return HttpResponse(text)
-
-# Страница "Обо мне"
-def about(request):
-    html_response = f"""
-    Имя: {author['first_name']}<br/>
-    Отчество: {author['middle_name']}<br/>
-    Фамилия: {author['last_name']}<br/>
-    Телефон: {author['phone']}<br/>
-    Email: {author['email']}
-    """
-    return HttpResponse(html_response)
-
 # Список товаров
 items = [
     {"id": 1, "name": "Кроссовки abibas", "quantity": 10},
@@ -37,6 +18,31 @@ items = [
     {"id": 4, "name": "Картофель фри", "quantity": 20},
     {"id": 5, "name": "Кепка", "quantity": 8}
 ]
+
+# Главная страница
+def home(request):
+    text = f"""
+    <h1>Изучаем django</h1>
+    <strong>Автор</strong>: <i>{author['last_name']} {author['first_name'][0]}.{author['middle_name'][0]}.</i>
+    <br><br>
+    <a href="/about/">Обо мне</a> | 
+    <a href="/items/">Список товаров</a>
+    """
+    return HttpResponse(text)
+
+# Страница "Обо мне"
+def about(request):
+    html_response = f"""
+    <h2>Обо мне:</h2>
+    Имя: {author['first_name']}<br/>
+    Отчество: {author['middle_name']}<br/>
+    Фамилия: {author['last_name']}<br/>
+    Телефон: {author['phone']}<br/>
+    Email: {author['email']}<br/><br/>
+    <a href="/">На главную</a> | 
+    <a href="/items/">Список товаров</a>
+    """
+    return HttpResponse(html_response)
 
 # Подробности товара по его ID
 def item_detail(request, item_id):
@@ -48,15 +54,21 @@ def item_detail(request, item_id):
     
     if found_item is not None:
         return HttpResponse(
-            f"<h1>{found_item['name']}</h1><p><a href='/items/'>Назад к списку товаров</a></p>"
+            f"<h1>{found_item['name']}</h1>"
+            f"<p>Количество: {found_item['quantity']} шт.</p>"
+            f"<p><a href='/items/'>Назад к списку товаров</a></p>"
         )
     else:
-        return HttpResponse("Товар не найден")
+        return HttpResponse(
+            f"<h3>Товар с id={item_id} не найден</h3>"
+            f"<p><a href='/items/'>Назад к списку товаров</a></p>"
+        )
 
 # Список всех товаров
 def all_items(request):
-    response_html = "<h1>Список товаров</h1><table border='1'><tr><th>Название</th><th>Количество</th></tr>"
+    response_html = "<h1>Список товаров</h1><ol>"
     for item in items:
-        response_html += f"<tr><td><a href='/item/{item['id']}/'>{item['name']}</a></td><td>{item['quantity']}</td></tr>"
-    response_html += "</table>"
+        response_html += f"<li><a href='/item/{item['id']}/'>{item['name']}</a> (в наличии: {item['quantity']} шт.)</li>"
+    response_html += "</ol>"
+    response_html += "<br/><a href='/'>На главную</a> | <a href='/about/'>Обо мне</a>"
     return HttpResponse(response_html)
